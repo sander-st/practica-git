073b77ddcb9de61bcdb1befd2c93599113e3d58e
Sander <code.nodejs.vh@gmail.com>
2021-02-07 20:38:12 +0000

    se ha modificado el saludo al usuario

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
 
+let modals = document.getElementsByClassName('modal')
 
-document.getElementById('modal').addEventListener('click', ()=> {
-    let modal = document.querySelector('.modal-container-screen')
+for(let i of modals) {
+    i.addEventListener('click', e => {
+        let user = e.target.dataset.name,
+            userCotainer = document.querySelectorAll('.user-name'),
+            modal = document.querySelector('.modal-container-screen')
+            // cambiar nombre
+            for(let i of userCotainer) i.textContent = user
+            // activar modal
+            modal.classList.add('show-modal')
+            // remover modal
+            modal.addEventListener('click', e => {
+                let target = e.target
+                if(target == modal) modal.classList.remove('show-modal')
+            })
 
-    // activando modal
-    modal.classList.add('show-modal')
-
-    modal.addEventListener('click', e => {
-        let target = e.target
-        if(target == modal)
-            modal.classList.remove('show-modal')
     })
-})
-
+}
