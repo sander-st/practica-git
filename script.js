802998c30e513d4b19d8307f89b91862cd0616ce
Sander <code.nodejs.vh@gmail.com>
2021-02-07 19:39:17 +0000

    se agrego dinamica al modal con js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// alert('Hi, Sander')
+
+
+
+document.getElementById('modal').addEventListener('click', ()=> {
+    let modal = document.querySelector('.modal-container-screen')
+
+    // activando modal
+    modal.classList.add('show-modal')
+
+    modal.addEventListener('click', e => {
+        let target = e.target
+        if(target == modal)
+            modal.classList.remove('show-modal')
+    })
+})
+
